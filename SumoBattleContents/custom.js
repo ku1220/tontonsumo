7ded9c624889fa11d1437d4bd464a8f457dcd78c
大阪 CNS <oosakacns@oosakanoMacBook-Pro.local>
2025-11-05 00:42:57 +0900

    render対応 custom.js修正

--- SumoBattleContents/custom.js
+++ SumoBattleContents/custom.js
@@ -1,4 +1,5 @@
-const PORT = 8082;
+// ローカルサーバー対応版
+/*const PORT = 8082;
 const IP = "192.168.0.6";
 
 window.GetWebSocketURLFromExternal = function () {
@@ -13,4 +14,24 @@ window.GetQRCodeURLFromExternal = function () {
 
   var qrUrl = `http://${IP}:${PORT}/Mobile/SumoBattleSend.html?id=`;
   return qrUrl + acid;
+};*/
+
+
+// GitHub、render.com対応版
+let acid = Date.now(); // unique ID （乱数でもOK）
+
+window.GetWebSocketURLFromExternal = function () {
+
+  // Render.com のURLから自動取得
+  var wsUri = `wss://${location.host}/?id=${acid}`;
+
+  console.log("📡 [custom.js] WebSocket URL:", wsUri);
+  return wsUri;
+};
+
+window.GetQRCodeURLFromExternal = function () {
+
+  // PCが開いているURLを元に生成すればOK
+  var qrUrl = `https://${location.host}/Mobile/SumoBattleSend.html?id=${acid}`;
+  return qrUrl;
 };
